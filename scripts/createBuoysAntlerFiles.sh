d33ec363499d47b504147e4671cd1af171ad9034
Clément Aubry <clement.aubry@gmail.com>
2015-08-05 16:06:38 +0200

    [moos] Updating scripts and config files to fit with "buoy on board" feature.
    When a buoy is on board, this buoy has to launch the mission by publishing MISSION_MODEM_START=true
    It will be broadcasted with pShare via multicast

--- scripts/createBuoysAntlerFiles.sh
+++ scripts/createBuoysAntlerFiles.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 TIME_WARP=1
-NB_BUOY=2
+NB_BUOY=4
+BUOY_ON_BOARD=0
 
 while :
 do
   case $1 in
   -h | --help | -\?)
-    printf "%s [nb_buoy] [time_warp] \n" $0
     printf "  --nb_buoy=value : default is $NB_BUOY, \n\t should be in [2 ; 4], \n\t else write meta_(NB_BUOY)buoys.moos \n\t(shortcuts: -nb) \n"
+    printf "  --onBoard=value : default is $BUOY_ON_BOARD, \n\t should be in [0 ; nb_buoy], \n\t 0 if no buoy on board \n"
     printf "  --time_warp=value : default is $TIME_WARP \n\t(shortcuts: -t) \n"
     printf "  --help \n\t(shortcuts: -h, -?) \n" 
     exit 0
@@ -18,8 +19,13 @@ do
     printf "\tNew parameter NB_BUOY: %d\n" $NB_BUOY
     shift
     ;;
+  --onBoard=*)
+    BUOY_ON_BOARD=${1#*=}
+    printf "\tNew parameter BUOY_ON_BOARD: %d\n" $BUOY_ON_BOARD
+    shift
+    ;;
   -t=* | --time_warp=*)
-    TIME_WARP=${2#*=}
+    TIME_WARP=${1#*=}
     printf "\tNew parameter TIME_WARP: %d\n" $TIME_WARP
     shift
     ;;
@@ -50,9 +56,13 @@ for ((i=1; i<=$NB_BUOY; i++)); do
       SERVER_HOST=$SERVER_HOST \
       SERVER_PORT=$SERVER_PORT \
       TIME_WARP=$TIME_WARP "
-  nsplug meta_$[NB_BUOY]buoys.moos targ_$[NB_BUOY]buoys_AUV$i.moos -f ${PARAMETERS}
+    if [[ $i == $BUOY_ON_BOARD ]]; then
+      nsplug meta_$[NB_BUOY]buoys_onBoard.moos targ_$[NB_BUOY]buoys_AUV$[i]_onBoard.moos -f ${PARAMETERS}
+    else
+      nsplug meta_$[NB_BUOY]buoys.moos targ_$[NB_BUOY]buoys_AUV$[i].moos -f ${PARAMETERS}
+    fi
+  
 done
-
 
 
 printf "done\n" ; sleep 1
